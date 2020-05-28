9ffd6d28ebd2e0959aaba365867ea3887d9a7fd4
Freekheldens <freekheldens@hotmail.com>
2020-05-28 23:15:40 +0200

    In Game Music will now play when started game via main menu scene

--- Project Vrij 2/Assets/Scripts/MainMenu.cs
+++ Project Vrij 2/Assets/Scripts/MainMenu.cs
@@ -1,24 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnStartGameButtonPressed()
     {
         SceneLoader.Instance.GoToPinBoard();
+        FMODUnity.RuntimeManager.PlayOneShot("event:/In Game Music");
     }
 
     public void OnSettingsButtonPressed()
